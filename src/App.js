10a8450d2c09e31ebefbf8abe5d4872e297cfbe7
PatricCormac <socolov1897@gmail.com>
2020-07-23 20:49:09 +0500

    Асинхронный запрос данных для таблицы и лоадер

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
+import Loader from "./Loader/Loader";
 
 class App extends Component {
+  async componentDidMount() {
+    const response = await fetch(
+      "http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}"
+    );
+    const data = await response.json();
+    console.log(data);
+  }
+
   render() {
-    return <div className="container">MyApp</div>;
+    return (
+      <div className="container">
+        <Loader />
+      </div>
+    );
   }
 }
 
---
+++ src/Loader/Loader.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Loader.css";
+
+export default () => (
+  <div class="lds-ripple">
+    <div></div>
+    <div></div>
+  </div>
+);
