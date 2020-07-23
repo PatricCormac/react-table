65aaedfe05ec2c37277133c477042bf67f4dbdf1
PatricCormac <socolov1897@gmail.com>
2020-07-24 01:56:46 +0500

    Стилизованный ReactPaginate

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Table from "./Table/Table";
 import DeteilRowView from "./DeteilRowView/DeteilRowView";
 import ModeSelector from "./ModeSelector/ModeSelector";
 import _ from "lodash";
+import ReactPaginate from "react-paginate";
 
 class App extends Component {
   state = {
@@ -39,8 +40,12 @@ class App extends Component {
     });
     this.fetchData(url);
   };
+  pageChangeHandler = (page) => {
+    console.log(page);
+  };
 
   render() {
+    const pageSize = 50;
     if (!this.state.isModeSelected) {
       return (
         <div className="container">
@@ -62,6 +67,26 @@ class App extends Component {
           />
         )}
         {this.state.row ? <DeteilRowView person={this.state.row} /> : null}
+        {this.state.data.length > pageSize ? (
+          <ReactPaginate
+            previousLabel={"<"}
+            nextLabel={">"}
+            breakLabel={"..."}
+            breakClassName={"break-me"}
+            pageCount={20}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={this.pageChangeHandler}
+            containerClassName={"pagination"}
+            activeClassName={"active"}
+            pageClassName="page-item"
+            pageLinkClassName="page-link"
+            previousClassName="page-item"
+            nextClassName="page-item"
+            previousLinkClassName="page-link"
+            nextLinkClassName="page-link"
+          />
+        ) : null}
       </div>
     );
   }
