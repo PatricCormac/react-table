1a2878e46404f40cc10a1b62c518fa78a23938a4
PatricCormac <socolov1897@gmail.com>
2020-07-26 22:56:29 +0500

    Updates

---
+++ precache-manifest.075adf4ecaa98b199d2b70b76c74a7dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3915bfd468ee611c88aea870858eb5ff",
+    "url": "/react-table/index.html"
+  },
+  {
+    "revision": "74dcc485039a6de6f0c5",
+    "url": "/react-table/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "34d30190278aaaebc2df",
+    "url": "/react-table/static/css/main.1c80f26d.chunk.css"
+  },
+  {
+    "revision": "74dcc485039a6de6f0c5",
+    "url": "/react-table/static/js/2.cdc5ca1a.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/react-table/static/js/2.cdc5ca1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34d30190278aaaebc2df",
+    "url": "/react-table/static/js/main.90f4d730.chunk.js"
+  },
+  {
+    "revision": "13ef5d0acb97c42f0dbc",
+    "url": "/react-table/static/js/runtime-main.0aa6a7ef.js"
+  }
+]);
